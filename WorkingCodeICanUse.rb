d90c54cacf1bce5470f5e23e00447da80c82da1b
lexalexander10 <l.alexander10@gmail.com>
2015-01-13 17:08:14 -0800

    finally figured out how to submit a form, follow those links, scrape those links and then add them to a csv file.

--- WorkingCodeICanUse.rb
+++ WorkingCodeICanUse.rb
@@ -3,38 +3,124 @@ require "csv"
 
 class Miami 
 
-	def sale_scraper(search) 
+	def sale_scraper(query) 
 		agent = Mechanize.new 
-		page = agent.get("http://miami.craigslist.org")
+		page = agent.get("http://chicago.craigslist.org")
 		form = page.form_with(:action => "/search/")	
-		form.field_with(:value => "").value = search
-		agent.submit(form)
+		form.field_with(:value => "").value = query.capitalize
+		submit = agent.submit(form)
+		links = submit.links 
 		search_results = []
-			current_page = agent.uri 
-			current_page.links.each do |links| 
-			attributes = links.attributes.attributes["class"]
-			search_results << links if attributes && attributes.value == "hdrlnk"
+			links.each do |results| 
+				attributes = results.attributes.attributes["class"]
+				search_results << results if attributes && attributes.value = "hdrlnk"
 			end 
-                                              
-		CSV.open("craigslistTesting.csv", "wb") do |csv| 
-			search_results.each do |searches| 
-				click = searches.click 
-				doc  = click.parser
-				doc.css(".postingtitle").each do |parse|
-					csv << [parse.text].collect(&:strip)
+			
+			CSV.open("resultsForSale#{query}.csv", "wb") do |csv| 
+				search_results.each do |searches| 
+				clicked_links = searches.click
+				pages = agent.get(clicked_links)
+				parse = pages.parser 
+				 
+				 	parse.css(".postingtitle").each do |title|
+						csv << [title.text].collect(&:strip)
+					end 
+					parse.css("#postingbody").each do |body|
+						csv << [body.text].collect(&:strip)
+					end 
+					
 				end 
-				doc.css("#postingbody").each do |parse|
-					csv << [parse.text].collect(&:strip) 
-				end 	
+
+			end 	
+		# search_results = []
+		# 	current_page = agent.get(URI) 
+		# 	current_page.links.each do |links| 
+		# 	attributes = links.attributes.attributes["class"]
+		# 	search_results << links if attributes && attributes.value == "hdrlnk"
+		# 	end 
+		# search_results.each do |search| 
+		# 	click_links = search.click 
+		# 	doc  = click_links.parser 	
+  #                            pp doc 
+  #                       end                  
+		# CSV.open("craigslistTesting.csv", "wb") do |csv| 
+		# 	search_results.each do |searches| 
+		# 		click = searches.click 
+		# 		doc  = click.parser
+		# 		doc.css(".postingtitle").each do |parse|
+		# 			csv << [parse.text].collect(&:strip)
+		# 		end 
+		# 		doc.css("#postingbody").each do |parse|
+		# 			csv << [parse.text].collect(&:strip) 
+		# 		end 	
+		# 	end 
+		# 	puts "I'm done"
+		# end                            
+		puts "Done scraping #{query} in For Sale category done"                                                             
+
+	end 
+
+	def events_scraper(query) 
+		agent = Mechanize.new 
+		page = agent.get("http://chicago.craigslist.org")
+		form = page.form_with(:action => "/search/")	
+		form.field_with(:value => "").value = query.capitalize
+		submit = agent.submit(form)
+		links = submit.links 
+		search_results = []
+			links.each do |results| 
+				attributes = results.attributes.attributes["class"]
+				search_results << results if attributes && attributes.value = "hdrlnk"
 			end 
-			puts "I'm done"
-		end                                                                                         
+			
+			CSV.open("resultsForSale#{query}.csv", "wb") do |csv| 
+				search_results.each do |searches| 
+				clicked_links = searches.click
+				pages = agent.get(clicked_links)
+				parse = pages.parser 
+				 
+				 	parse.css(".postingtitle").each do |title|
+						csv << [title.text].collect(&:strip)
+					end 
+					parse.css("#postingbody").each do |body|
+						csv << [body.text].collect(&:strip)
+					end 
+					
+				end 
+
+			end 	
+		# search_results = []
+		# 	current_page = agent.get(URI) 
+		# 	current_page.links.each do |links| 
+		# 	attributes = links.attributes.attributes["class"]
+		# 	search_results << links if attributes && attributes.value == "hdrlnk"
+		# 	end 
+		# search_results.each do |search| 
+		# 	click_links = search.click 
+		# 	doc  = click_links.parser 	
+  #                            pp doc 
+  #                       end                  
+		# CSV.open("craigslistTesting.csv", "wb") do |csv| 
+		# 	search_results.each do |searches| 
+		# 		click = searches.click 
+		# 		doc  = click.parser
+		# 		doc.css(".postingtitle").each do |parse|
+		# 			csv << [parse.text].collect(&:strip)
+		# 		end 
+		# 		doc.css("#postingbody").each do |parse|
+		# 			csv << [parse.text].collect(&:strip) 
+		# 		end 	
+		# 	end 
+		# 	puts "I'm done"
+		# end                            
+		puts "Done scraping #{query} in events category"                                                             
 
 	end 
 
 end
 	miami = Miami.new
-	miami.sale_scraper("nike") 
+	miami.sale_scraper("horses") 
+
 
 # miami_forms.field_with(:value => "").value = "laptops"
 # ##We're assigning the field require
